68ff5dc6d71b2b6a22c50c3b1de656437a74fe0e
Ahmed F. LAGHA <laghaahmedfouad@gmail.com>
2024-11-07 22:47:44 +0100

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,59 +1,60 @@
 #!/usr/bin/python3
-"""N Queens"""
-import sys
+"""
+The N queens puzzle is the challenge of placing N
+non-attacking queens on anN×N chessboard
+"""
 
 
-def print_board(board, n):
-    """Print allocated positions to the queen"""
-    b = []
+from sys import argv
+
+if __name__ == "__main__":
+    sol = []
+    if len(argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    if argv[1].isdigit() is False:
+        print("N must be a number")
+        exit(1)
+    n = int(argv[1])
+    if n < 4:
+        print("N must be at least 4")
+        exit(1)
 
     for i in range(n):
-        for j in range(n):
-            if j == board[i]:
-                b.append([i, j])
-    print(b)
+        sol.append([i, None])
 
+    def already_exists(y):
+        """check that a queen does not already exist in that y value"""
+        for x in range(n):
+            if y == sol[x][1]:
+                return True
+        return False
 
-def is_position_safe(board, i, j, r):
-    """Checks if the position is safe for the queen"""
-    return board[i] in (j, j - i + r, i - r + j)
+    def reject(x, y):
+        """determines whether or not to reject the solution"""
+        if (already_exists(y)):
+            return False
+        i = 0
+        while (i < x):
+            if abs(sol[i][1] - y) == abs(i - x):
+                return False
+            i += 1
+        return True
 
+    def clear_a(x):
+        """clears the answers from the point of failure on"""
+        for i in range(x, n):
+            sol[i][1] = None
 
-def safe_positions(board, row, n):
-    """Find all safe positions where the queen can be allocated"""
-    if row == n:
-        print_board(board, n)
+    def nqueens(x):
+        """recursive backtracking function to find the solution"""
+        for y in range(n):
+            clear_a(x)
+            if reject(x, y):
+                sol[x][1] = y
+                if (x == n - 1):
+                    print(sol)
+                else:
+                    nqueens(x + 1)
 
-    else:
-        for j in range(n):
-            allowed = True
-            for i in range(row):
-                if is_position_safe(board, i, j, row):
-                    allowed = False
-            if allowed:
-                board[row] = j
-                safe_positions(board, row + 1, n)
-
-
-def create_board(size):
-    """Generates the board"""
-    return [0 * size for i in range(size)]
-
-
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    exit(1)
-
-try:
-    n = int(sys.argv[1])
-except BaseException:
-    print("N must be a number")
-    exit(1)
-
-if (n < 4):
-    print("N must be at least 4")
-    exit(1)
-
-board = create_board(int(n))
-row = 0
-safe_positions(board, row, int(n))
+    nqueens(0)
